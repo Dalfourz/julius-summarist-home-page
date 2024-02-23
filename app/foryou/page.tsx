71c0a9523f746378c8feb0d3a8ee80af43677f25
Julius Handojo <yjulius.mail@gmail.com>
2024-02-23 17:22:06 +1100

    finalising for you page design

--- app/foryou/page.tsx
+++ app/foryou/page.tsx
@@ -1,9 +1,13 @@
+import Recommended from "../ui/foryou/Recommended";
 import Selected from "../ui/foryou/Selected";
+import Suggested from "../ui/foryou/Suggested";
 
 export default function ForYou() {
   return (
     <div>
       <Selected />
+      <Recommended />
+      <Suggested />
     </div>
   )
 }
